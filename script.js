11b8fae824c949cad2a1c518992a84e5e44ad383
uShire <afgregana@up.edu.ph>
2024-04-02 15:21:39 +0800

    first commit home page

---
+++ script.js
@@ -0,0 +1,8 @@
+ const headerNav = document.querySelector(".header__nav");
+ const toggleMenu = document.querySelector(".toggle__menu");
+
+ toggleMenu.addEventListener("click",()=>{
+    headerNav.classList.toggle("open");
+    toggleMenu.classList.toggle("open");
+    
+ });
